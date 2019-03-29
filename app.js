7a509fcb316adc71d72bb22115b569b3450038db
ambikarajesh <ambikula@gmail.com>
2019-03-28 17:24:50 -0700

    paginated using express.paginate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const paginate = require('express-paginate');
 
 const ejs = require('ejs');
 const mongoDB_key = require('./keys');
@@ -11,6 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(paginate.middleware(2,50));
 app.use('/', imageRouter)
 mongoose.connect(encodeURI(mongoDB_key)).then(result =>{
     app.listen(3000);
--- controllers/image.js
+++ controllers/image.js
@@ -1,12 +1,22 @@
+const paginate = require('express-paginate');
 const Image = require('../models/image');
-exports.getImages = (req, res, next) =>{
-    Image.find().then(images =>{
-        res.render('image-list', {
-            title:'Gallery',
-            images:images
-        })
-    }).catch(err=> console.log(err))
-   
+exports.getImages = async(req, res, next) =>{
+    const [ results, itemCount ] = await Promise.all([
+            Image.find({}).limit(req.query.limit).skip(req.skip).lean().exec(),
+            Image.count({})
+        ]);
+    const pageCount = Math.ceil(itemCount / req.query.limit);
+      
+    res.render('image-list', {
+        title:'Gallery',
+        images: results,
+        pageCount,
+        itemCount,
+        pages: paginate.getArrayPages(req)(3, pageCount, req.query.page),
+        currentPage : req.query.page,
+        hasPrevPage: paginate.hasPreviousPages,
+        hasNextPage:paginate.hasNextPages(pageCount),
+    })  
 }
 
 exports.getImage = (req, res, next) =>{
